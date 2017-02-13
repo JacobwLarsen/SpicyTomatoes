ebe8dd6be23fbf1c034c007fdc521268171f2046
JacobwLarsen <jacobwlarsendk@gmail.com>
2017-02-13 15:33:10 +0100

    updated comments

--- media.py
+++ media.py
@@ -1,23 +1,25 @@
 import webbrowser
 
 
-#create class: Movie
+# create class: Movie
 class Movie():
 
 
-    """Documentation"""
-
+    """Initializes the class "Movie" to create movie objects
+       that have a title, storyline, poster image, and playable trailer"""
+    
+    # Valid ratings to be given to Movies (currently unused)
     VALID_RATINGS=["G","PG","PG13","R"]
 
-    #define the initializing constructor for class: Movie
+    # define the initializing constructor for class: Movie
     def __init__(self,movie_title,movie_storyline, poster_image, trailer_youtube):
-        #print("Movie Constructor Called")
+        # initilizing instance variables with passed parameters from entertainment.py
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url =trailer_youtube
 
-    #open web browser
+    # open web browser to play trailer from youtube url
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
